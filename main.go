LucasRoesler/faas-swarm

commit 25d168301e01d73974a35aa569ec71ca54beceab

Update faas-provider to 0.13.1
**What**
- Update faas-provider to 0.13.1
- Udate the types configuration reading to use the standardized env
  parsing from faas-provider. Additionally replace BootstrapConfig with
  a SwarmConfig.  This embeds the standardized FaaSConfig so that it can
  be used in the function proxy.
- Remove unused config code and tests
- Add new dev.sh contrib file and new `start-dev` and `stop-dev` make
  commands to help standardized testing commands for local development
  and testing.  Prometheus configuration from `faas` project are also
  copied to enable to basic local development

This can be tested locally using

```
make build start-dev
echo -n localdev | faas-cli login --username=admin --password-stdin
faas-cli deploy  --fprocess=cat -e write_debug=true --image=functions/alpine --name echo-me
echo "test message" | faas-cli invoke echo-me
faas-cli log echo-me
```

Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

Gopkg.toml
@@ -8,7 +8,7 @@
 
 [[constraint]]
   name = "github.com/openfaas/faas-provider"
-  version = "0.10.1"
+  version = "0.13.1"
 
 [[constraint]]
   name = "github.com/docker/go-units"

vendor/github.com/openfaas/faas-provider/serve.go
@@ -65,6 +65,8 @@ func Serve(handlers *types.FaaSHandlers, config *types.FaaSConfig) {
 	r.HandleFunc("/system/secrets", handlers.SecretHandler).Methods(http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete)
 	r.HandleFunc("/system/logs", handlers.LogHandler).Methods(http.MethodGet)
 
+	r.HandleFunc("/system/namespaces", handlers.ListNamespaceHandler).Methods("GET")
+
 	// Open endpoints
 	r.HandleFunc("/function/{name:["+NameExpression+"]+}", handlers.FunctionProxy)
 	r.HandleFunc("/function/{name:["+NameExpression+"]+}/", handlers.FunctionProxy)

vendor/github.com/openfaas/faas-provider/types/config.go
@@ -5,13 +5,20 @@ import (
 	"time"
 )
 
+const (
+	defaultReadTimeout  = 10 * time.Second
+	defaultMaxIdleConns = 1024
+)
+
 // FaaSHandlers provide handlers for OpenFaaS
 type FaaSHandlers struct {
-	FunctionReader http.HandlerFunc
-	DeployHandler  http.HandlerFunc
 	// FunctionProxy provides the function invocation proxy logic.  Use proxy.NewHandlerFunc to
 	// use the standard OpenFaaS proxy implementation or provide completely custom proxy logic.
-	FunctionProxy  http.HandlerFunc
+	FunctionProxy http.HandlerFunc
+
+	FunctionReader http.HandlerFunc
+	DeployHandler  http.HandlerFunc
+
 	DeleteHandler  http.HandlerFunc
 	ReplicaReader  http.HandlerFunc
 	ReplicaUpdater http.HandlerFunc
@@ -19,18 +26,57 @@ type FaaSHandlers struct {
 	// LogHandler provides streaming json logs of functions
 	LogHandler http.HandlerFunc
 
-	// Optional: Update an existing function
-	UpdateHandler http.HandlerFunc
-	HealthHandler http.HandlerFunc
-	InfoHandler   http.HandlerFunc
+	// UpdateHandler an existing function/service
+	UpdateHandler        http.HandlerFunc
+	HealthHandler        http.HandlerFunc
+	InfoHandler          http.HandlerFunc
+	ListNamespaceHandler http.HandlerFunc
 }
 
 // FaaSConfig set config for HTTP handlers
 type FaaSConfig struct {
-	TCPPort         *int
-	ReadTimeout     time.Duration
-	WriteTimeout    time.Duration
-	EnableHealth    bool
+	// TCPPort is the public port for the API.
+	TCPPort *int
+	// HTTP timeout for reading a request from clients.
+	ReadTimeout time.Duration
+	// HTTP timeout for writing a response from functions.
+	WriteTimeout time.Duration
+	// EnableHealth enables/disables the default health endpoint bound to "/healthz".
+	EnableHealth bool
+	// EnableBasicAuth enforces basic auth on the API. If set, reads secrets from file-system
+	// location specificed in `SecretMountPath`.
 	EnableBasicAuth bool
+	// SecretMountPath specifies where to read secrets from for embedded basic auth.
 	SecretMountPath string
+	// MaxIdleConns with a default value of 1024, can be used for tuning HTTP proxy performance.
+	MaxIdleConns int
+	// MaxIdleConnsPerHost with a default value of 1024, can be used for tuning HTTP proxy performance.
+	MaxIdleConnsPerHost int
+}
+
+// GetReadTimeout is a helper to safely return the configured ReadTimeout or the default value of 10s
+func (c *FaaSConfig) GetReadTimeout() time.Duration {
+	if c.ReadTimeout <= 0*time.Second {
+		return defaultReadTimeout
+	}
+	return c.ReadTimeout
+}
+
+// GetMaxIdleConns is a helper to safely return the configured MaxIdleConns or the default value of 1024
+func (c *FaaSConfig) GetMaxIdleConns() int {
+	if c.MaxIdleConns < 1 {
+		return defaultMaxIdleConns
+	}
+
+	return c.MaxIdleConns
+}
+
+// GetMaxIdleConns is a helper to safely return the configured MaxIdleConns or the default value which
+// should then match the MaxIdleConns
+func (c *FaaSConfig) GetMaxIdleConnsPerHost() int {
+	if c.MaxIdleConnsPerHost < 1 {
+		return c.GetMaxIdleConns()
+	}
+
+	return c.MaxIdleConnsPerHost
 }

vendor/github.com/openfaas/faas-provider/types/read_config.go
@@ -1,0 +1,113 @@
+package types
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+// OsEnv implements interface to wrap os.Getenv
+type OsEnv struct {
+}
+
+// Getenv wraps os.Getenv
+func (OsEnv) Getenv(key string) string {
+	return os.Getenv(key)
+}
+
+// HasEnv provides interface for os.Getenv
+type HasEnv interface {
+	Getenv(key string) string
+}
+
+// ReadConfig constitutes config from env variables
+type ReadConfig struct {
+}
+
+// ParseIntValue parses the the int in val or, if there is an error, returns the
+// specified default value
+func ParseIntValue(val string, fallback int) int {
+	if len(val) > 0 {
+		parsedVal, parseErr := strconv.Atoi(val)
+		if parseErr == nil && parsedVal >= 0 {
+			return parsedVal
+		}
+	}
+	return fallback
+}
+
+// ParseIntOrDurationValue parses the the duration in val or, if there is an error, returns the
+// specified default value
+func ParseIntOrDurationValue(val string, fallback time.Duration) time.Duration {
+	if len(val) > 0 {
+		parsedVal, parseErr := strconv.Atoi(val)
+		if parseErr == nil && parsedVal >= 0 {
+			return time.Duration(parsedVal) * time.Second
+		}
+	}
+
+	duration, durationErr := time.ParseDuration(val)
+	if durationErr != nil {
+		return fallback
+	}
+
+	return duration
+}
+
+// ParseBoolValue parses the the boolean in val or, if there is an error, returns the
+// specified default value
+func ParseBoolValue(val string, fallback bool) bool {
+	if len(val) > 0 {
+		return val == "true"
+	}
+	return fallback
+}
+
+// ParseString verifies the string in val is not empty. When empty, it returns the
+// specified default value
+func ParseString(val string, fallback string) string {
+	if len(val) > 0 {
+		return val
+	}
+	return fallback
+}
+
+// Read fetches config from environmental variables.
+func (ReadConfig) Read(hasEnv HasEnv) (*FaaSConfig, error) {
+	cfg := &FaaSConfig{
+		ReadTimeout:  ParseIntOrDurationValue(hasEnv.Getenv("read_timeout"), time.Second*10),
+		WriteTimeout: ParseIntOrDurationValue(hasEnv.Getenv("write_timeout"), time.Second*10),
+		// default value from Gateway
+		EnableBasicAuth: ParseBoolValue(hasEnv.Getenv("basic_auth"), false),
+		// default value from Gateway
+		SecretMountPath: ParseString(hasEnv.Getenv("secret_mount_path"), "/run/secrets/"),
+	}
+
+	port := ParseIntValue(hasEnv.Getenv("port"), 8080)
+	cfg.TCPPort = &port
+
+	cfg.MaxIdleConns = 1024
+	maxIdleConns := hasEnv.Getenv("max_idle_conns")
+	if len(maxIdleConns) > 0 {
+		val, err := strconv.Atoi(maxIdleConns)
+		if err != nil {
+			return nil, fmt.Errorf("invalid value for max_idle_conns: %s", maxIdleConns)
+		}
+		cfg.MaxIdleConns = val
+
+	}
+
+	cfg.MaxIdleConnsPerHost = 1024
+	maxIdleConnsPerHost := hasEnv.Getenv("max_idle_conns_per_host")
+	if len(maxIdleConnsPerHost) > 0 {
+		val, err := strconv.Atoi(maxIdleConnsPerHost)
+		if err != nil {
+			return nil, fmt.Errorf("invalid value for max_idle_conns_per_host: %s", maxIdleConnsPerHost)
+		}
+		cfg.MaxIdleConnsPerHost = val
+
+	}
+
+	return cfg, nil
+}

types/read_config.go
@@ -3,91 +3,30 @@
 package types
 
 import (
-	"os"
-	"strconv"
-	"time"
+	ftypes "github.com/openfaas/faas-provider/types"
 )
-
-// OsEnv implements interface to wrap os.Getenv
-type OsEnv struct {
-}
-
-// Getenv wraps os.Getenv
-func (OsEnv) Getenv(key string) string {
-	return os.Getenv(key)
-}
-
-// HasEnv provides interface for os.Getenv
-type HasEnv interface {
-	Getenv(key string) string
-}
 
 // ReadConfig constitutes config from env variables
 type ReadConfig struct {
 }
 
-func parseIntValue(val string, fallback int) int {
-	if len(val) > 0 {
-		parsedVal, parseErr := strconv.Atoi(val)
-		if parseErr == nil && parsedVal >= 0 {
-			return parsedVal
-		}
-	}
-	return fallback
-}
-
-func parseIntOrDurationValue(val string, fallback time.Duration) time.Duration {
-	if len(val) > 0 {
-		parsedVal, parseErr := strconv.Atoi(val)
-		if parseErr == nil && parsedVal >= 0 {
-			return time.Duration(parsedVal) * time.Second
-		}
-	}
-
-	duration, durationErr := time.ParseDuration(val)
-	if durationErr != nil {
-		return fallback
-	}
-
-	return duration
-}
-
-func parseBoolValue(val string, fallback bool) bool {
-	switch val {
-	case "1", "true":
-		return true
-	case "0", "false":
-		return false
-	default:
-		return fallback
-	}
-}
-
 // Read fetches config from environmental variables.
-func (ReadConfig) Read(hasEnv HasEnv) BootstrapConfig {
-	cfg := BootstrapConfig{}
+func (ReadConfig) Read(hasEnv ftypes.HasEnv) (SwarmConfig, error) {
+	cfg := SwarmConfig{}
 
-	readTimeout := parseIntOrDurationValue(hasEnv.Getenv("read_timeout"), time.Second*10)
-	writeTimeout := parseIntOrDurationValue(hasEnv.Getenv("write_timeout"), time.Second*10)
+	faasCfg, err := ftypes.ReadConfig{}.Read(hasEnv)
+	if err != nil {
+		return cfg, err
+	}
 
-	const defaultPort = 8080
+	cfg.DNSRoundRobin = ftypes.ParseBoolValue(hasEnv.Getenv("dnsrr"), false)
+	cfg.FaaSConfig = *faasCfg
 
-	cfg.TCPPort = parseIntValue(hasEnv.Getenv("port"), defaultPort)
-	cfg.ReadTimeout = readTimeout
-	cfg.WriteTimeout = writeTimeout
-
-	cfg.EnableBasicAuth = parseBoolValue(hasEnv.Getenv("basic_auth"), false)
-	cfg.DNSRoundRobin = parseBoolValue(hasEnv.Getenv("dnsrr"), false)
-
-	return cfg
+	return cfg, nil
 }
 
-// BootstrapConfig for the process.
-type BootstrapConfig struct {
-	ReadTimeout     time.Duration
-	WriteTimeout    time.Duration
-	TCPPort         int
-	EnableBasicAuth bool
+// SwarmConfig contains the configuration for the process.
+type SwarmConfig struct {
 	// DNSRoundRobin controls how faas-swarm will lookup functions when proxying requests.
 	// When
 	//	DNSRoundRobin = true
@@ -96,4 +35,6 @@ type BootstrapConfig struct {
 	// 	DNSRoundRObin = false
 	// faas-swarm will attempt to resolve the function by name, validating using the Swarm API
 	DNSRoundRobin bool
+	// FaasConfig contains the standard OpenFaaS provider configuration
+	FaaSConfig ftypes.FaaSConfig
 }

Makefile
@@ -7,6 +7,15 @@ build:
 test-unit:
 	go test -v $(go list ./... | grep -v /vendor/) -cover
 
+
+.PHONY: start-dev
+start-dev:
+	cd contrib && ./dev.sh
+
+.PHONY: stop-dev
+stop-dev:
+	docker stack rm func
+
 .PHONY: build-armhf
 build-armhf:
 	docker build --build-arg http_proxy="${http_proxy}" --build-arg https_proxy="${https_proxy}" -t openfaas/faas-swarm:$(TAG)-armhf . -f Dockerfile.armhf

contrib/dev.sh
@@ -1,0 +1,17 @@
+#!/bin/sh
+
+export BASIC_AUTH="true"
+export AUTH_URL="http://basic-auth-plugin:8080/validate"
+
+# Secrets should be created even if basic-auth is disabled.
+echo "Attempting to create credentials for gateway.."
+echo "admin" | docker secret create basic-auth-user -
+echo "localdev" | docker secret create basic-auth-password -
+
+
+docker stack deploy func --compose-file docker-compose.yml
+
+
+printf '%-15s:\t %s\n' 'Username' 'admin'
+printf '%-15s:\t %s\n' 'Password' 'localdev'
+printf '%-15s:\t %s\n' 'CLI Auth' 'echo -n localdev | faas-cli login --username=admin --password-stdin'

contrib/docker-compose.yml
@@ -1,0 +1,232 @@
+version: "3.3"
+services:
+    gateway:
+        ports:
+            - 8080:8080
+        image: openfaas/gateway:0.17.3
+        networks:
+            - functions
+        environment:
+            functions_provider_url: "http://faas-swarm:8080/"
+            read_timeout: "5m5s" # Maximum time to read HTTP request
+            write_timeout: "5m5s" # Maximum time to write HTTP response
+            upstream_timeout: "5m" # Maximum duration of upstream function call - should be more than read_timeout and write_timeout
+            dnsrr: "true" # Temporarily use dnsrr in place of VIP while issue persists on PWD
+            faas_nats_address: "nats"
+            faas_nats_port: 4222
+            direct_functions: "true" # Functions are invoked directly over the overlay network
+            direct_functions_suffix: ""
+            basic_auth: "${BASIC_AUTH:-true}"
+            secret_mount_path: "/run/secrets/"
+            scale_from_zero: "true" # Enable if you want functions to scale from 0/0 to min replica count upon invoke
+            max_idle_conns: 1024
+            max_idle_conns_per_host: 1024
+            auth_proxy_url: "${AUTH_URL:-}"
+            auth_proxy_pass_body: "false"
+        deploy:
+            resources:
+                # limits:   # Enable if you want to limit memory usage
+                #     memory: 200M
+                reservations:
+                    memory: 100M
+            restart_policy:
+                condition: on-failure
+                delay: 5s
+                max_attempts: 20
+                window: 380s
+            placement:
+                constraints:
+                    - "node.platform.os == linux"
+        secrets:
+            - basic-auth-user
+            - basic-auth-password
+
+    # auth service provide basic-auth plugin for system APIs
+    basic-auth-plugin:
+        image: openfaas/basic-auth-plugin:0.17.0
+        networks:
+            - functions
+        environment:
+            secret_mount_path: "/run/secrets/"
+            user_filename: "basic-auth-user"
+            pass_filename: "basic-auth-password"
+        deploy:
+            placement:
+                constraints:
+                    - "node.role == manager"
+                    - "node.platform.os == linux"
+            resources:
+                # limits:   # Enable if you want to limit memory usage
+                #     memory: 100M
+                reservations:
+                    memory: 50M
+            restart_policy:
+                condition: on-failure
+                delay: 5s
+                max_attempts: 20
+                window: 380s
+        secrets:
+            - basic-auth-user
+            - basic-auth-password
+
+    # Docker Swarm provider
+    faas-swarm:
+        volumes:
+            - "/var/run/docker.sock:/var/run/docker.sock"
+        image: openfaas/faas-swarm:latest-dev
+        networks:
+            - functions
+        environment:
+            read_timeout: "5m5s" # set both here, and on your functions
+            write_timeout: "5m5s" # set both here, and on your functions
+            DOCKER_API_VERSION: "1.30"
+            basic_auth: "${BASIC_AUTH:-true}"
+            secret_mount_path: "/run/secrets/"
+        deploy:
+            placement:
+                constraints:
+                    - "node.role == manager"
+                    - "node.platform.os == linux"
+            resources:
+                # limits:   # Enable if you want to limit memory usage
+                #     memory: 100M
+                reservations:
+                    memory: 100M
+            restart_policy:
+                condition: on-failure
+                delay: 5s
+                max_attempts: 20
+                window: 380s
+        secrets:
+            - basic-auth-user
+            - basic-auth-password
+
+    nats:
+        image: nats-streaming:0.11.2
+        # Uncomment the following port mappings if you wish to expose the
+        # NATS client and/or management ports you must also add `-m 8222` to the command
+        # ports:
+        #     - 4222:4222
+        #     - 8222:8222
+        command: "--store memory --cluster_id faas-cluster"
+        networks:
+            - functions
+        deploy:
+            resources:
+                limits:
+                    memory: 125M
+                reservations:
+                    memory: 50M
+            placement:
+                constraints:
+                    - "node.platform.os == linux"
+
+    queue-worker:
+        image: openfaas/queue-worker:0.8.0
+        networks:
+            - functions
+        environment:
+            max_inflight: "1"
+            ack_wait: "5m5s" # Max duration of any async task / request
+            basic_auth: "${BASIC_AUTH:-true}"
+            secret_mount_path: "/run/secrets/"
+            gateway_invoke: "true"
+            faas_gateway_address: "gateway"
+        deploy:
+            resources:
+                limits:
+                    memory: 50M
+                reservations:
+                    memory: 20M
+            restart_policy:
+                condition: on-failure
+                delay: 5s
+                max_attempts: 20
+                window: 380s
+            placement:
+                constraints:
+                    - "node.platform.os == linux"
+        secrets:
+            - basic-auth-user
+            - basic-auth-password
+
+    # End services
+
+    # Start monitoring
+
+    prometheus:
+        image: prom/prometheus:v2.11.0
+        environment:
+            no_proxy: "gateway"
+        configs:
+            - source: prometheus_config
+              target: /etc/prometheus/prometheus.yml
+            - source: prometheus_rules
+              target: /etc/prometheus/alert.rules.yml
+        command:
+            - "--config.file=/etc/prometheus/prometheus.yml"
+        #   - '-storage.local.path=/prometheus'
+        ports:
+            - 9090:9090
+        networks:
+            - functions
+        deploy:
+            placement:
+                constraints:
+                    - "node.role == manager"
+                    - "node.platform.os == linux"
+            resources:
+                limits:
+                    memory: 500M
+                reservations:
+                    memory: 200M
+
+    alertmanager:
+        image: prom/alertmanager:v0.18.0
+        environment:
+            no_proxy: "gateway"
+        command:
+            - "--config.file=/alertmanager.yml"
+            - "--storage.path=/alertmanager"
+        networks:
+            - functions
+        # Uncomment the following port mapping if you wish to expose the Prometheus
+        # Alertmanager UI.
+        # ports:
+        #     - 9093:9093
+        deploy:
+            resources:
+                limits:
+                    memory: 50M
+                reservations:
+                    memory: 20M
+            placement:
+                constraints:
+                    - "node.role == manager"
+                    - "node.platform.os == linux"
+        configs:
+            - source: alertmanager_config
+              target: /alertmanager.yml
+        secrets:
+            - basic-auth-password
+
+configs:
+    prometheus_config:
+        file: ./prometheus/prometheus.yml
+    prometheus_rules:
+        file: ./prometheus/alert.rules.yml
+    alertmanager_config:
+        file: ./prometheus/alertmanager.yml
+
+networks:
+    functions:
+        driver: overlay
+        attachable: true
+        labels:
+            - "openfaas=true"
+
+secrets:
+    basic-auth-user:
+        external: true
+    basic-auth-password:
+        external: true

commit 99a05ded468c93d8ac3a0f0e800c6f9a2f22cddf

Bump faas-provider to 0.13.2
**What**
- Bump faas-provider to 0.13.2 to fix the regression introduced in #60
  related to the health handler config

Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

Gopkg.toml
@@ -8,7 +8,7 @@
 
 [[constraint]]
   name = "github.com/openfaas/faas-provider"
-  version = "0.13.1"
+  version = "0.13.2"
 
 [[constraint]]
   name = "github.com/docker/go-units"

main.go
@@ -52,8 +52,6 @@ func main() {
 
 	funcProxyHandler := handlers.NewFunctionLookup(dockerClient, cfg.DNSRoundRobin)
 
-	cfg.FaaSConfig.EnableHealth = true // Needed due to breaking change in #534
-
 	bootstrapHandlers := bootTypes.FaaSHandlers{
 		DeleteHandler:  handlers.DeleteHandler(dockerClient),
 		DeployHandler:  handlers.DeployHandler(dockerClient, maxRestarts, restartDelay),
@@ -72,7 +70,6 @@ func main() {
 		ReadTimeout:     cfg.FaaSConfig.GetReadTimeout(),
 		WriteTimeout:    cfg.FaaSConfig.WriteTimeout,
 		TCPPort:         cfg.FaaSConfig.TCPPort,
-		EnableHealth:    cfg.FaaSConfig.EnableHealth,
 		EnableBasicAuth: cfg.FaaSConfig.EnableBasicAuth,
 		SecretMountPath: cfg.FaaSConfig.SecretMountPath,
 	}

vendor/github.com/openfaas/faas-provider/serve.go
@@ -72,7 +72,7 @@ func Serve(handlers *types.FaaSHandlers, config *types.FaaSConfig) {
 	r.HandleFunc("/function/{name:["+NameExpression+"]+}/", handlers.FunctionProxy)
 	r.HandleFunc("/function/{name:["+NameExpression+"]+}/{params:.*}", handlers.FunctionProxy)
 
-	if config.EnableHealth {
+	if handlers.HealthHandler != nil {
 		r.HandleFunc("/healthz", handlers.HealthHandler).Methods("GET")
 	}
 

vendor/github.com/openfaas/faas-provider/types/config.go
@@ -27,7 +27,9 @@ type FaaSHandlers struct {
 	LogHandler http.HandlerFunc
 
 	// UpdateHandler an existing function/service
-	UpdateHandler        http.HandlerFunc
+	UpdateHandler http.HandlerFunc
+	// HealthHandler defines the default health endpoint bound to "/healthz
+	// If the handler is not set, then the "/healthz" path will not be configured
 	HealthHandler        http.HandlerFunc
 	InfoHandler          http.HandlerFunc
 	ListNamespaceHandler http.HandlerFunc
@@ -42,6 +44,9 @@ type FaaSConfig struct {
 	// HTTP timeout for writing a response from functions.
 	WriteTimeout time.Duration
 	// EnableHealth enables/disables the default health endpoint bound to "/healthz".
+	//
+	// Deprecated: basic auth is enabled automatcally by setting the HealthHandler in the FaaSHandlers
+	// struct.  This value is not longer read or used.
 	EnableHealth bool
 	// EnableBasicAuth enforces basic auth on the API. If set, reads secrets from file-system
 	// location specificed in `SecretMountPath`.

vendor/github.com/openfaas/faas-provider/types/read_config.go
@@ -76,9 +76,8 @@ func ParseString(val string, fallback string) string {
 // Read fetches config from environmental variables.
 func (ReadConfig) Read(hasEnv HasEnv) (*FaaSConfig, error) {
 	cfg := &FaaSConfig{
-		ReadTimeout:  ParseIntOrDurationValue(hasEnv.Getenv("read_timeout"), time.Second*10),
-		WriteTimeout: ParseIntOrDurationValue(hasEnv.Getenv("write_timeout"), time.Second*10),
-		// default value from Gateway
+		ReadTimeout:     ParseIntOrDurationValue(hasEnv.Getenv("read_timeout"), time.Second*10),
+		WriteTimeout:    ParseIntOrDurationValue(hasEnv.Getenv("write_timeout"), time.Second*10),
 		EnableBasicAuth: ParseBoolValue(hasEnv.Getenv("basic_auth"), false),
 		// default value from Gateway
 		SecretMountPath: ParseString(hasEnv.Getenv("secret_mount_path"), "/run/secrets/"),